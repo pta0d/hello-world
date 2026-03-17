eab59fae50bf1f3288d36e729534d079066aab9e
pta0d <133895720+pta0d@users.noreply.github.com>
2026-03-17 16:36:18 +0200

    Refactor name input logic into askname function

--- mysandbox/sandbox_py/test_issue_link.py
+++ mysandbox/sandbox_py/test_issue_link.py
@@ -1,11 +1,15 @@
 # test_issue_link.py
 
+def askname():
+  name = input('What's your name?\n> ')
+  if name == '':
+    print('Enter non-null string for name! Try again.')
+  return name
+
 def main():
   name = ''
-  while name != ''
-    name = input('What's your name?\n> ')
-    if name == '':
-      print('Enter non-null string for name! Try again.')
+  while name != '':
+    name = askname()
   print(f'Hello, ${name}!')
 
 if __name__ = '__main__':
