654e01e508a4e9eb4a169d85b8576ff5d81a7027
pta0d <133895720+pta0d@users.noreply.github.com>
2026-03-17 16:09:26 +0200

    Add test_issue_link.py for user name input
    I'll test issue on lines

---
+++ mysandbox/sandbox_py/test_issue_link.py
@@ -0,0 +1,12 @@
+# test_issue_link.py
+
+def main():
+  name = ''
+  while name != ''
+    name = input('What's your name?\n> ')
+    if name == '':
+      print('Enter non-null string for name! Try again.')
+  print(f'Hello, ${name}!')
+
+if __name__ = '__main__':
+  main()
